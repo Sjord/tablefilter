ee3a199c936c8418d7271e52a8cf915a7ac97bc7
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2017-05-04 15:54:53 +0200

    Add bookmarklet file
    Bookmarklet just loads tablefilter.js in the current page.
    Don't include link to bookmarklet because Github doesn't support that.

---
+++ bookmarklet.js
@@ -0,0 +1,6 @@
+javascript:(function () { 
+    var s = document.createElement('script');
+    s.setAttribute("type", "text/javascript");
+    s.setAttribute("src", "https://rawgit.com/Sjord/tablefilter/master/tablefilter.js");
+    document.body.appendChild(s);
+})();
